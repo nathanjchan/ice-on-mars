25f16ee140aad7bb909307626cb38bb93dcea960
Nathan Chan <nathanjchan2@gmail.com>
2020-07-24 01:26:40 -0700

    Add parallel computing, adjust GLCM features

--- randomForest.R
+++ randomForest.R
@@ -1,21 +1,25 @@
 # Set Up ----
 df = read.csv("radar.csv", stringsAsFactors = FALSE)
 df = df[, !(colnames(df) %in% "X")]
-df = df[df$tif != "Radar_Images/tiff/s_0240xx/s_02407601_tiff.tif",]
-set.seed(23*3)
+bad = c("Radar_Images/tiff/s_0240xx/s_02407601_tiff.tif", "Radar_Images/tiff/s_0564xx/s_05645702_tiff.tif")
+df = df[!(df$tif %in% bad),]
 
 library(raster)
 library(e1071)
-library(randomForest)
-#library(spatial)
 library(glcm)
-#library(reticulate)
-# use_python("C:/Users/natha/anaconda3/python.exe")
-#import("keras")
+library(parallel)
+library(randomForest)
+
+
+
+
+
+
+
+
 
 
 # Functions ----
-# CHANGE FOR REGRESSION
 sampleTIF = function(n) {
   # Given sample size n, return a sample of radargrams from GLOBAL VARIABLE df
   
@@ -88,9 +92,9 @@ replaceInf = function(x) {
 }
 
 
-getGLCM = function(radar_smol) {
+getGLCM = function(radar_smol, window_size) {
   # Given a radargram, calculate the GLCM and return the feature statistics
-  gray = glcm(radar_smol)
+  gray = glcm(radar_smol, window = c(window_size, window_size))
   correlation = sapply(as.matrix(gray$glcm_correlation), replaceInf)
   stats = c(getStatistics(as.matrix(gray$glcm_mean)),
             getStatistics(as.matrix(gray$glcm_variance)),
@@ -105,7 +109,6 @@ getGLCM = function(radar_smol) {
 }
 
 
-# CHANGE FOR REGRESSION
 extractFeatures = function(tif_path) {
   # Given file path to a .tif file, return a list of features
   print(tif_path)
@@ -124,88 +127,103 @@ extractFeatures = function(tif_path) {
   
   # CLASSIFICATION ----
   # ice or not
-  if (df$depth[df$tif == tif_path] == -32768) {
-    features[1] = "no"
-  } else {
-    features[1] = "yes"
-  }
+  #if (df$depth[df$tif == tif_path] == -32768) {
+  #  features[1] = "no"
+  #} else {
+  #  features[1] = "yes"
+  #}
+  
+  features[1] = tif_path
   
   # REGRESSION ----
   # shallow ice depth
   #features[1] = df$depth[df$tif == tif_path]
   
-  # STATISTICS ----
+  # COLOR STATISTICS ----
   features = append(features, getStatistics(radar_mat))
-  
-  
-  # GRAY LEVEL CO-OCCURRANCE MATRIX ----
-  stats = getGLCM(radar_smol)
-  stats2 = getGLCM(radar_smol2)
-  features = append(features, stats)
-  features = append(features, stats2)
   
   # COLOR HISTOGRAM ----
   color_hist = hist(radar_mat, breaks = seq(0, 255, l = 26))
   features = append(features, color_hist$density) # density instead of counts
   
-  color_hist2 = hist(radar_mat, breaks = seq(0, 255, l = 26))
-  features = append(features, color_hist2$density) # density instead of counts
-  
-  color_hist3 = hist(radar_mat, breaks = seq(0, 255, l = 26))
-  features = append(features, color_hist3$density) # density instead of counts
+  # GRAY LEVEL CO-OCCURRANCE MATRIX ----
+  stats1 = getGLCM(radar_smol, window_size = 3)
+  stats2 = getGLCM(radar_smol2, window_size = 3)
+  stats3 = getGLCM(radar_smol, window_size = 9)
+  features = append(features, stats1)
+  features = append(features, stats2)
+  features = append(features, stats3)
   
   # END ----
   end_time = Sys.time()
   time_taken = end_time - start_time
   total_time_taken = end_time - global_start_time
-  global_i <<- global_i + 1
+  #global_i <<- global_i + 1
   print(time_taken)
   print(total_time_taken)
-  print(paste0(global_i, "/", global_n))
+  #print(paste0(global_i, "/", global_n))
   return(features)
 }
 
 
-# Sample data frame ----
+extractIceClassification = function(tif_path) {
+  # CLASSIFICATION
+  # ice or not
+  if (df$depth[df$tif == tif_path] == -32768) {
+    ice_or_not = "no"
+  } else {
+    ice_or_not = "yes"
+  }
+  return(ice_or_not)
+}
+
+
+
+
+
+
+
+# Sample radargrams----
 n = 500
 global_i = 0
 global_n = n
 global_start_time = Sys.time()
+set.seed(23*3)
 sample = sampleTIF(n)
 half1 = 1:(n/2)
 half2 = (n/2 + 1):n
 
-# create data frame
-features = lapply(sample$tif, extractFeatures)
+
+# PARALLEL???
+num_cores = detectCores()
+cl = makeCluster(num_cores, outfile = "output.txt")
+clusterExport(cl, varlist = c("relevantColumns", "getStatistics", "replaceInf", "getGLCM", "global_start_time"))
+clusterEvalQ(cl, {
+  library(raster)
+  library(e1071)
+  library(glcm)
+})
+features = parLapply(cl, sample$tif, extractFeatures)
+stopCluster(cl)
+
+
+ice = lapply(sample$tif, extractIceClassification)
+#features = lapply(sample$tif, extractFeatures)
 features_df = as.data.frame(do.call(rbind, features))
+features_df = features_df[, !(colnames(features_df) %in% "V1")]
+ice_df = as.data.frame(do.call(rbind, ice))
+features_df = cbind(ice_df, features_df)
+
 
 # rename features
-feature_names = c("ice", "mean", "sd", "skew", "kurtosis",
-                  paste0("glcm_mean", 1:4),
-                  paste0("glcm_variance", 1:4),
-                  paste0("glcm_homogeneity", 1:4),
-                  paste0("glcm_contrast", 1:4),
-                  paste0("glcm_dissimilarity", 1:4),
-                  paste0("glcm_entropy", 1:4),
-                  paste0("glcm_second_moment", 1:4),
-                  paste0("glcm_correlation", 1:4),
-                  paste0("glcm_mean2", 1:4),
-                  paste0("glcm_variance2", 1:4),
-                  paste0("glcm_homogeneity2", 1:4),
-                  paste0("glcm_contrast2", 1:4),
-                  paste0("glcm_dissimilarity2", 1:4),
-                  paste0("glcm_entropy2", 1:4),
-                  paste0("glcm_second_moment2", 1:4),
-                  paste0("glcm_correlation2", 1:4),
-                  paste0("color_hist_one", 1:25),
-                  paste0("color_hist_two", 1:25),
-                  paste0("color_hist_three", 1:25))
+feature_names = c("ice", "mean", "sd", "skew", "kurt", paste0("color_hist", 1:25), paste0("glcm", 1:96))
 num_features = length(feature_names)
 if (num_features != ncol(features_df)) {
   stop("Number of feature names and number of features don't match!")
 }
 colnames(features_df) = feature_names
 big = features_df[, colnames(features_df) %in% feature_names]
+write.csv(big, "big.csv")
 
 # convert to numeric
 # NOTE: REMOVE WHEN DOING REGRESSION
@@ -242,17 +260,17 @@ round(importance(big_rf), 2)
 
 # Testing ----
 start.time <- Sys.time()
-###
+
 radar = raster("Radar_Images/tiff/s_0357xx/s_03576301_tiff.tif")
 # crop by selecting middle 3000
 relevant = relevantColumns(ncol(radar), 3000)
 e = extent(relevant[1] - 1, relevant[3000], 0, nrow(radar))
 radar_crop = crop(radar, e)
 # shrink the resolution by x20
-radar_smol = aggregate(radar_crop, fact=100)
-gray = glcm(radar_smol)
+radar_smol = aggregate(radar_crop, fact=200)
+gray = glcm(radar_smol, window = c(3, 3))
 plot(gray)
-###
+
 end.time <- Sys.time()
 time.taken <- end.time - start.time
 time.taken
@@ -265,4 +283,4 @@ plot(radar)
 color_hist = hist(radar_mat)
 
 
-extractFeatures("Radar_Images/tiff/s_0357xx/s_03576301_tiff.tif")
+test = extractFeatures("Radar_Images/tiff/s_0357xx/s_03576301_tiff.tif")
