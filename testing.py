dc81b429528bfeacb16508b52af4b0d9cf3bc36a
Nathan Chan <nathanjchan2@gmail.com>
2020-07-29 02:53:43 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,44 @@
+# https://www.tensorflow.org/tutorials/keras/classification
 
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+# Import the Fashion MNIST dataset
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Explore the data
+train_images.shape
+len(train_labels)
+train_labels
+test_images.shape
+len(test_labels)
+
+# Preprocess the data
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
