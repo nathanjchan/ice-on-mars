4fc6aa49cb0a11f2f90cfeb185ab6d9ec8018aad
Nathan Chan <nathanjchan2@gmail.com>
2020-08-14 12:44:07 -0700

    Create plotting.R

---
+++ plotting.R
@@ -0,0 +1,15 @@
+library(ggmap)
+
+sample = read.csv("sample.csv")
+sample = sample[-c(22, 26, 42, 54),]
+
+world_coords = c(-180,-80, 180, 80)
+world_map = get_stamenmap(world_coords, zoom = 1, maptype = "toner-lite")
+
+ggmap(world_map) +
+  geom_point(aes(x = center_long - 180, y = center_lat, color = big$ice), data = sample, size = 3) +
+  labs(title = "Actual", color = "Ice")
+
+ggmap(world_map) +
+  geom_point(aes(x = center_long - 180, y = center_lat, color = rf_cv$predicted$`270`), data = sample, size = 3) +
+  labs(title = "Predicted", color = "Ice")
