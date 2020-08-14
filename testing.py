8dbf7d023f1e440bc39a428ddff3038468cfeafc
Nathan Chan <nathanjchan2@gmail.com>
2020-08-14 12:48:16 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,44 +1,39 @@
-# https://www.tensorflow.org/tutorials/keras/classification
+# https://www.tensorflow.org/tutorials/quickstart/beginner
 
-# TensorFlow and tf.keras
 import tensorflow as tf
-from tensorflow import keras
 
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
+mnist = tf.keras.datasets.mnist
 
-print(tf.__version__)
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# Import the Fashion MNIST dataset
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
 
-# Explore the data
-train_images.shape
-len(train_labels)
-train_labels
-test_images.shape
-len(test_labels)
+predictions = model(x_train[:1]).numpy()
+predictions
 
-# Preprocess the data
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+tf.nn.softmax(predictions).numpy()
 
-train_images = train_images / 255.0
-test_images = test_images / 255.0
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+loss_fn(y_train[:1], predictions).numpy()
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+
+probability_model(x_test[:5])
