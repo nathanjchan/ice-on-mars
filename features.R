98f41612b8e7441a17900b50ba0f076e4ee7499b
nathanjchan <njchan@ucdavis.edu>
2020-10-01 18:43:28 -0700

    Use all images in sample

--- features.R
+++ features.R
@@ -8,7 +8,7 @@ library(randomForest)
 source("functions.R")
 
 # Global Variables ----
-df = read.csv("radar.csv", stringsAsFactors = FALSE)
+df = read.csv("radar2.csv", stringsAsFactors = FALSE)
 df = df[, !(colnames(df) %in% "X")]
 
 
@@ -17,14 +17,14 @@ df = df[, !(colnames(df) %in% "X")]
 
 
 # Sample ----
-n = 500
+n = 23650
 global_i = 0
 global_n = n
 global_start_time = Sys.time()
 set.seed(23*3)
 sample = sampleTifClassification(n)
-#sample = sampleTifRegression(n)
-# write.csv(sample, "sample.csv")
+# sample = sampleTifRegression(n)
+# write.csv(sample, "sample23650.csv")
 half1 = 1:(n/2)
 half2 = (n/2 + 1):n
 
@@ -39,17 +39,16 @@ clusterEvalQ(cl, {
   library(raster)
   library(e1071)
   library(glcm)
-  rasterOptions(tmpdir = "D:/Mars_Data/__raster__/")
+  rasterOptions(tmpdir = "D:/Mars_Data/__raster__/", tmptime = 2)
 })
 features = parLapply(cl, sample$tif, extractFeatures)
 stopCluster(cl)
 
-# # normal computing
-rasterOptions(tmpdir = "D:/Mars_Data/__raster__/")
-features = lapply(sample$tif, extractFeatures2)
+# normal computing
+# rasterOptions(tmpdir = "D:/Mars_Data/__raster__/")
+# features = lapply(sample$tif, extractFeatures)
 
 # create data frame
-#features = lapply(sample$tif, extractFeatures)
 features_df = as.data.frame(do.call(rbind, features))
 
 # get labels
@@ -59,8 +58,8 @@ ice_df = as.data.frame(do.call(rbind, ice))
 features_df = cbind(ice_df, features_df)
 
 # rename features
-# feature_names = c("ice", "mean", "sd", "skew", "kurt", "density", paste0("color_hist", 1:25), paste0("glcm", 1:192))
-feature_names = c("ice", paste0("glcm", 1:64))
+feature_names = c("ice", "mean", "sd", "skew", "kurt", paste0("color_hist", 1:32), paste0("glcm", 1:288))
+# feature_names = c("ice", paste0("glcm", 1:160))
 num_features = length(feature_names)
 if (num_features != ncol(features_df)) {
   stop("Number of feature names and number of features don't match!")
@@ -69,7 +68,7 @@ colnames(features_df) = feature_names
 big = features_df[, colnames(features_df) %in% feature_names]
 
 big$ice = as.factor(big$ice)
-write.csv(big, "bigClassification.csv")
+write.csv(big, "bigColorGLCM64-45-24038new.csv")
 
 
 
@@ -95,27 +94,6 @@ write.csv(big, "bigClassification.csv")
 
 
 
-
-# Random Forest ----
-#big_rf = randomForest(ice ~ ., data=big, importance=TRUE, proximity=TRUE)
-
-x_train = big[half1,2:num_features]
-y_train = big[half1,1]
-x_test = big[half2,2:num_features]
-y_test = big[half2,1]
-
-big_rf = randomForest(x=x_train, y=y_train, xtest=x_test, ytest=y_test, importance=TRUE, proximity=TRUE)
-print(big_rf)
-round(importance(big_rf), 2)
-
-
-
-
-
-
-
-
-
 # Testing ----
 start.time <- Sys.time()
 
@@ -132,20 +110,20 @@ radar_smol2 = aggregate(radar_crop, fact = 2)
 
 # three types
 gray1 = glcm(radar_smol2, window = c(3, 3))
-gray2 = glcm(radar_smol2, window = c(7, 7))
-gray3 = glcm(radar_smol2, window = c(15, 15))
-gray4 = glcm(radar_smol2, window = c(31, 31))
-gray5 = glcm(radar_smol2, window = c(63, 63))
-gray6 = glcm(radar_smol2, window = c(127, 127))
+# gray2 = glcm(radar_smol2, window = c(7, 7))
+# gray3 = glcm(radar_smol2, window = c(15, 15))
+# gray4 = glcm(radar_smol2, window = c(31, 31))
+# gray5 = glcm(radar_smol2, window = c(63, 63))
+# gray6 = glcm(radar_smol2, window = c(127, 127))
 # gray4 = glcm(radar_smol200, window = c(3, 3), shift = list(c(0,1), c(1,1), c(1,0), c(1,-1)))
 # gray5 = glcm(radar_smol200, window = c(7, 7), shift = list(c(0,1), c(1,1), c(1,0), c(1,-1)))
 # gray6 = glcm(radar_smol200, window = c(15, 15), shift = list(c(0,1), c(1,1), c(1,0), c(1,-1)))
 plot(gray1)
-plot(gray2)
-plot(gray3)
-plot(gray4)
-plot(gray5)
-plot(gray6)
+# plot(gray2)
+# plot(gray3)
+# plot(gray4)
+# plot(gray5)
+# plot(gray6)
 
 end.time <- Sys.time()
 time.taken <- end.time - start.time
@@ -161,7 +139,7 @@ time.taken
 
 # random stuff
 radar_mat = as.matrix(radar)
-plot(radar)
+plot(radar, col = grey.colors(10, start=0, end=1))
 color_hist = hist(radar_mat)
 
 
